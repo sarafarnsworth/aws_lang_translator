c05e8ecb686b2c0e7fecbb000a4f4d451a63c0ed
sarafarnsworth <saramarionfarnsworth@gmail.com>
2021-01-19 18:49:35 +0000

    Removed hard-coded parameters and replaced them with arguments; user now inputs phrase to translate.

--- translator.py
+++ translator.py
@@ -2,18 +2,19 @@ import boto3
 
 client = boto3.client('translate')
 
-def translate_text(): 
+def translate_text(text, source_language_code, target_language_code): 
+    client = boto3.client('translate')
     response = client.translate_text(
-        Text='Hello! My name is Sara. I am learning to create code using AWS.',
-        SourceLanguageCode='en', 
-        TargetLanguageCode='es'
-        #language codes available at https://docs.aws.amazon.com/translate/latest/dg/what-is.html
+        Text=text,
+        SourceLanguageCode=source_language_code, 
+        TargetLanguageCode=target_language_code
+        #Amazon Translate language codes available at https://docs.aws.amazon.com/translate/latest/dg/what-is.html
     )
     
     print(response)
     
 def main():
-    translate_text()
+    translate_text(input("Please enter the text you would like to translate:  "),'en','es')
 
 if __name__=="__main__":
     main()
