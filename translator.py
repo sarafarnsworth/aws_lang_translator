5d7ed13167a76a26461a84f8a32f16f7ccba0b13
saramarionfarnsworth@gmail.com <saramarionfarnsworth@gmail.com>
2021-01-19 19:14:02 +0000

    Used dictionary for keyword arguments

--- translator.py
+++ translator.py
@@ -4,13 +4,14 @@ def translate_text(**kwargs):
     client = boto3.client('translate')
     response = client.translate_text(**kwargs)
     print(response)
-    
+
+kwargs={
+       "Text":input("Please enter the text you would like to translate:  "),
+        "SourceLanguageCode":'en',
+        "TargetLanguageCode":'es'
+        }    
 def main():
-    translate_text(
-        Text=input("Please enter the text you would like to translate:  "),
-        SourceLanguageCode='en',
-        TargetLanguageCode='es'
-        )
+    translate_text(**kwargs)
 
 if __name__=="__main__":
     main()
