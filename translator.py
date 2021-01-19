8056963c09c94e3e5ae7ab5c5f87d9402ea4cf36
saramarionfarnsworth@gmail.com <saramarionfarnsworth@gmail.com>
2021-01-19 19:05:08 +0000

    Replaced positional args with kwargs

--- translator.py
+++ translator.py
@@ -1,20 +1,16 @@
 import boto3
 
-client = boto3.client('translate')
-
-def translate_text(text, source_language_code, target_language_code): 
+def translate_text(**kwargs): 
     client = boto3.client('translate')
-    response = client.translate_text(
-        Text=text,
-        SourceLanguageCode=source_language_code, 
-        TargetLanguageCode=target_language_code
-        #Amazon Translate language codes available at https://docs.aws.amazon.com/translate/latest/dg/what-is.html
-    )
-    
+    response = client.translate_text(**kwargs)
     print(response)
     
 def main():
-    translate_text(input("Please enter the text you would like to translate:  "),'en','es')
+    translate_text(
+        Text=input("Please enter the text you would like to translate:  "),
+        SourceLanguageCode='en',
+        TargetLanguageCode='es'
+        )
 
 if __name__=="__main__":
     main()
