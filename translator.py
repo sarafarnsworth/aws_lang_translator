1d7325046418eea4ff8cebff388a8730aaf1223a
saramarionfarnsworth@gmail.com <saramarionfarnsworth@gmail.com>
2021-01-20 16:21:31 +0000

    translator.py now iterates over list/dictionaries in translate_input.json

--- translator.py
+++ translator.py
@@ -15,22 +15,31 @@ parser.add_argument(
     
 args = parser.parse_args()
 
-# Functions
+# FUNCTIONS
+
+# Returns a dictionary containing the contents of the Input section in hte input file
 def open_input():
     with open(args.filename) as file_object:
         contents = json.load(file_object)
-        return contents['Input'][0]
+        return contents['Input']
 
+# Boto3 function to use Amazon Translate to translate the text and only return the Translated Text
 def translate_text(**kwargs): 
     client = boto3.client('translate')
     response = client.translate_text(**kwargs)
-    print(response) 
+    print(response['TranslatedText']) 
+
+# Loop to iterate over the json file
+def translate_loop():
+    input_text = open_input()
+    for item in input_text:
+        translate_text(**item)
 
 # Main Function - use to call other functions
 def main():
-    kwargs = open_input()
-    translate_text(**kwargs)
+    translate_loop()
 
 if __name__ == "__main__":
     main()
+    
     
