eb966d35cd55e0f904a5c02a20cb4670dc73513d
saramarionfarnsworth@gmail.com <saramarionfarnsworth@gmail.com>
2021-01-20 17:08:18 +0000

    Added validation of language code inputs

--- translator.py
+++ translator.py
@@ -33,7 +33,41 @@ def translate_text(**kwargs):
 def translate_loop():
     input_text = open_input()
     for item in input_text:
-        translate_text(**item)
+        if input_validation(item)== True:
+            translate_text(**item)
+        else:
+            raise SystemError
+
+# Input validation function
+def input_validation(item):
+    languages = ["af","sq","am","ar","az","bn","bs","bg","zh","zh-TW","hr","cs","da","fa-AF",
+                "nl","en","et","fi","fr","fr-CA","ka","de","el","ha","he","hi","hu","id","it",
+                "ja","ko","lv","ms","no","fa","ps","pl","pt","ro","ru","sr","sk","sl","so","es",
+                "sw","sv","tl","ta","th","tr","uk","ur","vi"
+                ]
+    json_input=item
+    SourceLanguageCode = json_input['SourceLanguageCode']
+    TargetLanguageCode = json_input['TargetLanguageCode']
+
+    if SourceLanguageCode == TargetLanguageCode:
+        print("The SourceLanguageCode is the same as the TargetLanguageCode - nothing to do")
+        return False
+    elif SourceLanguageCode not in languages and TargetLanguageCode not in languages:
+        print("Neither the SourceLanguageCode and TargetLanguageCode are valid - stopping")
+        return False
+    elif SourceLanguageCode not in languages:
+        print("The SourceLanguageCode is not valid - stopping")
+        return False
+    elif TargetLanguageCode not in languages:
+        print("The TargetLanguageCode is not valid - stopping")
+        return False
+    elif SourceLanguageCode in languages and TargetLanguageCode in languages:
+        print("The SourceLanguageCode and TargetLanguageCode are valid - proceeding")
+        return True
+    else:
+        print("There is an issue")
+        return False
+
 
 # Main Function - use to call other functions
 def main():
