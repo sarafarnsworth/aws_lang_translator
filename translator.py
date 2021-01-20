431c8ed91502224b28cb1dd499d267233a485151
saramarionfarnsworth@gmail.com <saramarionfarnsworth@gmail.com>
2021-01-20 15:58:18 +0000

    Updated translator.py to access inputs from json file translate_input.json

--- translator.py
+++ translator.py
@@ -1,49 +1,36 @@
-# To run, use arguments in the command line, like so:
-# python translator.py --text "This is some text to translate" --source-language-code en --target-language-code es
 
 import argparse
+import json
 import boto3
 
-# Define the parser variable to equal argparse.ArgumentParser()
+# Arguments
 parser = argparse.ArgumentParser(description="Provides translation between one source language and another of the same set of languages.")
-
-# Add each of the arguments using the parser.add_argument() method
 parser.add_argument(
-    '--text',
-    dest="Text",
+    '--file',
+    dest="filename",
     type=str,
-    help="The text to translate. The text string can be a maximum of 5,000 bytes long. Depending on your character set, this may be fewer than 5,000 characters",
+    help="The path to the input file. The file should be valid json",
     required=True
     )
-
-parser.add_argument(
-    '--source-language-code', 
-    dest="SourceLanguageCode", 
-    type=str, 
-    help="The language code for the language of the source text. The language must be a language supported by Amazon Translate.",
-    required=True
-    )
-
-parser.add_argument(
-    '--target-language-code',
-    dest="TargetLanguageCode",
-    type=str,
-    help="The language code requested for the language of the target text. The language must be a language support by Amazon Translate.",
-    required=True
-    )
-
-# This will inspect the command line, convert each argument to the appropriate type and then invoke the appropriate action.
+    
 args = parser.parse_args()
+
+# Functions
+def open_input():
+    with open(args.filename) as file_object:
+        contents = json.load(file_object)
+        return contents['Input'][0]
 
 def translate_text(**kwargs): 
     client = boto3.client('translate')
     response = client.translate_text(**kwargs)
-    print(response)
+    print(response) 
 
+# Main Function - use to call other functions
 def main():
-    # uses vars(), an inbuilt function which returns a dictionary object
-    translate_text(**vars(args))
+    kwargs = open_input()
+    translate_text(**kwargs)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     
